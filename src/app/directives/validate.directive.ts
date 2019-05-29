558d32e55fa8ba62568a5171ab541ed4c5bada73
Nathan-TheGeek <nathan99md@gmail.com>
2019-05-29 14:35:31 -0500

    2 files modified required working with error showing and hiding.

--- src/app/directives/validate.directive.ts
+++ src/app/directives/validate.directive.ts
@@ -1,23 +1,52 @@
-import { Directive, OnInit, OnChanges, ElementRef, Renderer2, HostBinding, HostListener, Input } from '@angular/core';
+import { Directive, OnInit, OnChanges, AfterViewInit, ElementRef, Renderer2, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appValidate]'
 })
-export class ValidateDirective implements OnInit, OnChanges {
+export class ValidateDirective implements OnInit, OnChanges, AfterViewInit {
   private setup = false;
   private errorDisplay: ElementRef;
+  private inputRef; 
   private errorMsg = '';
   @HostBinding('style.background-color')
   backgroundColor: string = 'none';
   @Input() required = false;
+  @Input() label: string;
+  @Input() id: string;
 
   constructor(private el: ElementRef, private r: Renderer2) { }
 
   ngOnInit() {
-    this.errorDisplay = this.r.createElement('span');
-    this.r.appendChild(this.el.nativeElement.parentNode, this.errorDisplay);
     this.setup = true;
   }
+
+  ngAfterViewInit() {
+    // Get parent of the original input element
+    let parent = this.el.nativeElement.parentNode;
+    this.inputRef = this.el.nativeElement;
+    // Create a div
+    let divElement = this.r.createElement('div');
+    // Add class "input-wrapper"
+    this.r.addClass(divElement, 'validated-field');
+    // Add the div, just before the input
+    this.r.insertBefore(parent, divElement, this.inputRef);
+    // Remove the input
+    this.r.removeChild(parent, this.inputRef);
+    // Remove the directive attribute (not really necessary, but just to be clean)
+    this.r.removeAttribute(this.inputRef, 'appValidate');
+    // add the label for the input.
+    let label = this.r.createElement('label');
+    this.r.setAttribute(label, 'for', this.id);
+    let labelText = this.r.createText(this.label);
+    this.r.appendChild(label, labelText); 
+    this.r.appendChild(divElement, label);
+    // Re-add the input inside the div
+    this.r.appendChild(divElement, this.inputRef);
+    // add the error display.
+    this.errorDisplay = this.r.createElement('span');
+    this.r.setStyle(this.errorDisplay, 'color', 'red');
+    this.r.appendChild(divElement, this.errorDisplay);
+}
 
   @HostListener('change') ngOnChanges() {
     if(this.setup) {
@@ -32,7 +61,6 @@ export class ValidateDirective implements OnInit, OnChanges {
     if (this.required && !value) {
       this.errorMsg = 'The field [' + fieldName + '] is required.';
     }
-    console.log(this.errorMsg.length);
     if(this.errorMsg.length > 0) {
       this.showError();
     } else  {
@@ -42,18 +70,19 @@ export class ValidateDirective implements OnInit, OnChanges {
 
   private showError() {
     const errorText = this.r.createText(this.errorMsg);
-    this.backgroundColor = 'red';
+    this.r.setStyle(this.inputRef, 'background-color', 'red');
     this.r.appendChild(this.errorDisplay, errorText);
     this.r.setStyle(this.errorDisplay, 'display', 'inline-block');
   }
 
   private hideError() {
-    console.log(this.errorDisplay);
-    const childElements = this.errorDisplay.nativeElement;
+    // get and remove any error messages.
+    const childElements = (<any>this.errorDisplay).childNodes;
     for (let child of childElements) {
       this.r.removeChild(this.errorDisplay.nativeElement, child);
     }
+    // revert style
     this.r.setStyle(this.errorDisplay, 'display', 'none');
-    this.backgroundColor = 'none';
+    this.r.removeStyle(this.inputRef, 'background-color');
   }
 }
